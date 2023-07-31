1165622d0ae56a3da46ebf688756e92a6c550918
Aryan@0914 <21cs038@charusat.edu.in>
2023-07-31 13:03:03 +0530

    contact us

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Navbar from "../Components/Navbar.js";
 import Footer from "../Components/Footer.js";
-import { Outlet } from "react-router-dom";  
+import { Outlet } from "react-router-dom"; 
+import Contactus from "../Components/Contactus.js"; 
 
 const Landing = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <Contactus/>
       <Footer />
     </>
   );
