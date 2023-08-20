d873f45db181f3d75ca4eec055bee7041cad41cf
Aryanop0914 <21cs038@charusat.edu.in>
2023-08-20 11:59:36 +0530

    w

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.js";
+import "./index.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "./Redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
